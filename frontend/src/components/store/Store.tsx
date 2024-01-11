c133c08ab260987cd9abbfbd65549e1fbbb346db
amirhosein <ah.t222amir22@gmail.com>
2024-01-12 00:20:12 +0330

    check for token in main route and redirect

--- frontend/src/components/store/Store.tsx
+++ frontend/src/components/store/Store.tsx
@@ -1,8 +1,14 @@
 import Header from "../layout/Header";
 import BookList from "./books/BookList";
 import Footer from "../layout/Footer";
+import { Navigate, useNavigate } from "react-router-dom";
 
 const Store = () => {
+  const token = localStorage.getItem("token");
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
+
   return (
     <>
       <Header />
