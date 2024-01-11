a3ae769a79157e749a26d8a67088579fbd315f7a
amirhosein <ah.t222amir22@gmail.com>
2024-01-12 00:18:45 +0330

    add necessary providers to main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,20 +3,25 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import ErrorPage from "./components/error/ErrorPage.tsx";
 import Store from "./components/store/Store.tsx";
-import SignInForm from "./components/forms/SignInForm.tsx";
 import SignUpForm from "./components/forms/SignUpForm.tsx";
 import { ChakraProvider } from "@chakra-ui/react";
+import LoginForm from "./components/forms/LoginForm.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   { path: "/", element: <Store />, errorElement: <ErrorPage /> },
-  { path: "/signin", element: <SignInForm /> },
+  { path: "/login", element: <LoginForm /> },
   { path: "/signup", element: <SignUpForm /> },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <RouterProvider router={router} />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <RouterProvider router={router} />
+      </ChakraProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
