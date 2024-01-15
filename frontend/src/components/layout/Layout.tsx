7b26fb4195820f289ee6bb99abd1c3c68c8c618a
amirhosein <ah.t222amir22@gmail.com>
2024-01-15 11:28:07 +0330

    change main Layout file

---
+++ frontend/src/components/layout/Layout.tsx
@@ -0,0 +1,32 @@
+import { Box, useToast } from "@chakra-ui/react";
+import { Navigate, Outlet } from "react-router-dom";
+import Header from "../layout/Header";
+import MainBg from "../../assets/images/bg.jpg";
+
+const Layout = () => {
+  const token = localStorage.getItem("token");
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return (
+    <Box
+      backgroundImage={`url(${MainBg})`}
+      backgroundRepeat="no-repeat"
+      backgroundSize="cover"
+      backgroundPosition="center"
+      height="100vh"
+      width="100%"
+      display="flex"
+      flexDirection="column"
+    >
+      <Header />
+
+      <Box as="main" flexGrow="1">
+        <Outlet />
+      </Box>
+    </Box>
+  );
+};
+
+export default Layout;
