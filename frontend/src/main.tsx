2b7d934b29dee3cc52e667fa7785c2a833bc6116
amirhosein <ah.t222amir22@gmail.com>
2024-01-15 11:24:35 +0330

    change routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,16 +2,40 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
 import ErrorPage from "./components/error/ErrorPage.tsx";
 import LoginForm from "./components/forms/LoginForm.tsx";
 import SignUpForm from "./components/forms/SignUpForm.tsx";
-import Store from "./components/store/Store.tsx";
+import Layout from "./components/layout/Layout.tsx";
+import Store from "./components/store";
+import OrdersPage from "./components/orders/index.tsx";
 
 const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
-  { path: "/", element: <Store />, errorElement: <ErrorPage /> },
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "",
+        element: <Navigate to="store" />,
+      },
+      {
+        path: "store",
+        element: <Store />,
+      },
+      {
+        path: "orders",
+        element: <OrdersPage />,
+      },
+    ],
+  },
   { path: "/login", element: <LoginForm /> },
   { path: "/signup", element: <SignUpForm /> },
 ]);
