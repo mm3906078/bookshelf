9706b4c83ab5a828d5a10d2d60b6ef606cbd5332
amirhosein <ah.t222amir22@gmail.com>
2024-01-15 11:29:22 +0330

    add comment modal + textarea + api call

---
+++ frontend/src/components/comments/AddCommentModal.tsx
@@ -0,0 +1,95 @@
+import {
+    Button,
+    Modal,
+    ModalBody,
+    ModalCloseButton,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
+    ModalOverlay,
+    Textarea,
+    useToast
+} from "@chakra-ui/react";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { useState } from "react";
+
+interface Props {
+  isOpen: boolean;
+  onClose: () => void;
+  bookId: string;
+}
+
+const AddCommentModal = (props: Props) => {
+  const toast = useToast();
+  const token = localStorage.getItem("token");
+  const [commentText, setCommentText] = useState("");
+
+  const mutation = useMutation({
+    mutationFn: () => {
+      return axios.post(`/comments/add/${props.bookId}`, commentText, {
+        headers: { Authorization: token },
+      });
+    },
+    onSuccess: () => {
+      toast({
+        title: "Comment Submitted Successfully!",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+      });
+      props.onClose();
+    },
+    // onError: (error: any) => {},
+  });
+
+  const addCommentHandler = () => {
+    mutation.mutate();
+  };
+
+  return (
+    <Modal isCentered isOpen={props.isOpen} onClose={props.onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        {/* <form onSubmit={handleSubmit(onSubmit)}> */}
+        <ModalHeader>Comment</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Textarea
+            value={commentText}
+            onChange={(e) => {
+              setCommentText(e.target.value);
+            }}
+            placeholder="write your comment..."
+            size="sm"
+          />
+          {/* <FormControl isInvalid={!!errors.author}>
+              <FormLabel htmlFor="author">author</FormLabel>
+              <Input
+                id="author"
+                placeholder="author"
+                {...register("author", {
+                  required: "author is required",
+                })}
+              />
+              <FormErrorMessage>
+                {errors.author && errors.author.message}
+              </FormErrorMessage>
+            </FormControl> */}
+        </ModalBody>
+
+        <ModalFooter>
+          <Button colorScheme="red" mr={3} onClick={props.onClose}>
+            Cancel
+          </Button>
+          <Button onClick={addCommentHandler} colorScheme="green">
+            Submit
+          </Button>
+        </ModalFooter>
+        {/* </form> */}
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default AddCommentModal;
