6860bbc5281251884e3607534809ab039d98877f
amirhosein <ah.t222amir22@gmail.com>
2024-01-15 21:50:10 +0330

    put search inputs and add book in book index.tsx

--- frontend/src/components/store/index.tsx
+++ frontend/src/components/store/index.tsx
@@ -1,16 +1,27 @@
 import {
   Box,
   Button,
+  Input,
   Spinner,
-  Text
+  Text,
+  useDisclosure,
 } from "@chakra-ui/react";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { useState } from "react";
 import { Book } from "../../models/models";
+import AddBookModal from "./books/AddBookModal";
 import BooksTable from "./books/BooksTable";
 
-
 const Store = () => {
+  const role = localStorage.getItem("role");
+  const {
+    isOpen: isAddBookModalOpen,
+    onOpen: onOpenAddBookModal,
+    onClose: onCloseAddBookModal,
+  } = useDisclosure();
+  const [bookName, setBookName] = useState("");
+  const [authorName, setAuthorName] = useState("");
   const { refetch, isFetching, data } = useQuery({
     queryKey: ["books"],
     queryFn: async (): Promise<Book[]> => {
@@ -18,6 +29,18 @@ const Store = () => {
       return data;
     },
   });
+
+  // const mutation = useMutation({
+  //   mutationFn: () => {
+  //     return axios.post("/books/search", {
+  //       author: authorName,
+  //       title: bookName,
+  //     });
+  //   },
+  //   onSuccess: (res) => {
+  //     console.log(res);
+  //   },
+  // });
 
   if (isFetching) {
     return (
@@ -40,34 +63,79 @@ const Store = () => {
   }
 
   return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      height="100%"
-    >
-      {!isFetching && data?.length === 0 && (
-        <Box
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          flexDirection="column"
-          gap="10px"
-        >
-          <Text color="white" fontWeight="700" fontSize="46px">
-            No Books Yet!
-          </Text>
-          <Button
-            onClick={() => {
-              refetch();
-            }}
+    <>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        height="100%"
+      >
+        <Box bg="white" padding="20px" borderRadius="10px">
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            mb="20px"
+            gap="10px"
           >
-            Refetch
-          </Button>
+            {role === "admin" && (
+              <Button color="green" onClick={onOpenAddBookModal}>
+                Add Book
+              </Button>
+            )}
+            <Box display="flex" alignItems="center" gap="10px">
+              <Input
+                value={bookName}
+                onChange={(e) => {
+                  setBookName(e.target.value);
+                }}
+                placeholder="Book name"
+              />
+              <Input
+                value={authorName}
+                onChange={(e) => {
+                  setAuthorName(e.target.value);
+                }}
+                placeholder="Author name"
+              />
+              <Button padding="10px 40px" onClick={() => {}}>
+                Search
+              </Button>
+            </Box>
+          </Box>
+
+          {!isFetching && data?.length === 0 && (
+            <Box
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              flexDirection="column"
+              gap="10px"
+            >
+              <Text fontWeight="700" fontSize="46px">
+                No Books Yet!
+              </Text>
+              <Button
+                colorScheme="blue"
+                onClick={() => {
+                  refetch();
+                }}
+              >
+                Refetch
+              </Button>
+            </Box>
+          )}
+          {data && data.length > 0 && <BooksTable books={data} />}
         </Box>
+      </Box>
+
+      {isAddBookModalOpen && (
+        <AddBookModal
+          isOpen={isAddBookModalOpen}
+          onClose={onCloseAddBookModal}
+        />
       )}
-      {data && data.length > 0 && <BooksTable books={data} />}
-    </Box>
+    </>
   );
 };
 
