bf4ce755f3a4678bb7269054815203f85b1086b9
Mreza.salehi <mm3906078@gmail.com>
2024-01-25 13:21:36 +0000

    dockerize frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,12 +5,13 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true,
+    host: '0.0.0.0',
+    port: 3000,
     proxy: {
-      "/users": "http://localhost:5000",
-      "/books": "http://localhost:5000",
-      "/orders": "http://localhost:5000",
-      "/comments": "http://localhost:5000",
+      "/users": "http://backend:5000",
+      "/books": "http://backend:5000",
+      "/orders": "http://backend:5000",
+      "/comments": "http://backend:5000",
     },
   },
 });
